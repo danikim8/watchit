79558bd7dca542900c263f424a4017feabb8f5c0
danikim8 <202115057@sangmyung.kr>
2025-04-13 21:37:43 +0900

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import Container from '@/components/Container';
+import Header from '@/components/Header';
+import { ThemeProvider } from '@/lib/ThemeContext';
+import '@/styles/globals.css';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>watchit</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider>
+        <Header />
+        <Container page>
+          <Component {...pageProps} />
+        </Container>
+      </ThemeProvider>
+    </>
+  );
+}
